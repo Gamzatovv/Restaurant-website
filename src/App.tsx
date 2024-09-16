a3be6bb1a64eb1fec5423127ceddce5eb3ce294f
igrok275849 <igrok963584@gmail.com>
2024-09-16 12:56:15 +0300

    create a footer and add it to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss'
 import Header from './components/blocks/Header/Header'
 import Main from './page/Main/Main'
-
+import Footer from './components/blocks/Footer/Footer'
 
 function App() {
 
@@ -9,6 +9,7 @@ function App() {
     <>
       <Header/>
       <Main/>
+      <Footer/>
     </>
   )
 }
